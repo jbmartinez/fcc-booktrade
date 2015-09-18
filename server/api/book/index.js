7147dcb8a9b9ebff92fe2fd174fc973dc90fa3c5
Juan Martínez <jbmartinezs@yahoo.com>
2015-09-18 16:10:33 +0000

    more bug fixes

--- server/api/book/index.js
+++ server/api/book/index.js
@@ -2,16 +2,17 @@
 
 var express = require('express');
 var controller = require('./book.controller');
+var auth = require('../../auth/auth.service');
 
 var router = express.Router();
 
 router.get('/', controller.index);
-router.get('/user/:id', controller.userBooks);
+router.get('/user/:id', auth.isAuthenticated(), controller.userBooks);
 router.get('/:id', controller.show);
 // router.get('/search/:q', controller.show);
-router.post('/', controller.create);
-router.put('/:id', controller.update);
-router.patch('/:id', controller.update);
-router.delete('/:id', controller.destroy);
+router.post('/', auth.isAuthenticated(), controller.create);
+router.put('/:id', auth.isAuthenticated(), controller.update);
+router.patch('/:id', auth.isAuthenticated(), controller.update);
+router.delete('/:id', auth.isAuthenticated(), controller.destroy);
 
 module.exports = router;
--- server/api/trade/index.js
+++ server/api/trade/index.js
@@ -2,15 +2,16 @@
 
 var express = require('express');
 var controller = require('./trade.controller');
+var auth = require('../../auth/auth.service');
 
 var router = express.Router();
 
 router.get('/', controller.index);
 router.get('/:id', controller.show);
-router.get('/user/:id', controller.hasUser);
-router.post('/', controller.create);
-router.put('/:id', controller.update);
-router.patch('/:id', controller.update);
-router.delete('/:id', controller.destroy);
+router.get('/user/:id', auth.isAuthenticated(), controller.hasUser);
+router.post('/', auth.isAuthenticated(), controller.create);
+router.put('/:id', auth.isAuthenticated(), controller.update);
+router.patch('/:id', auth.isAuthenticated(), controller.update);
+router.delete('/:id', auth.isAuthenticated(), controller.destroy);
 
 module.exports = router;
